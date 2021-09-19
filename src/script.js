eaa776fd3f4e6f63c0f146d68035706ef5cb67d3
kiwoon Kim <87232579+tigerenergy@users.noreply.github.com>
2021-09-19 18:58:42 +0900

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -114,7 +114,7 @@ window.addEventListener('resize', () =>
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.set(4, 1, - 4)
-scene.add(camera)
+scene.add(camera) 
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
